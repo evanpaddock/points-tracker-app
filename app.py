6df8ee5b51cef71884047b5d8b51abd680c7b05f
evanpaddock <edpaddock@crimson.ua.edu>
2023-10-05 15:11:07 -0500

    works good but sorting by timestamps wrong

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, request, render_template, redirect, url_for
+from flask import Flask, jsonify, request, render_template
 from operator import itemgetter
 import json
+from datetime import datetime
 
 # Each transaction will be in a format similar to {"payer": "DANNON", "points": 5000, "timestamp": "2020-11-02T14:00:00Z"}
 transactions = []
@@ -10,26 +11,33 @@ app = Flask(__name__)
 
 
 def spend_points(points_to_spend):
-    global total_points
+    global total_points, transactions
 
     total_points -= points_to_spend
 
     message = []
-    transactions_sorted_by_timestamp = sorted(transactions, key=itemgetter("timestamp"))
+    transactions_sorted_by_timestamp = sorted(
+        transactions, key=itemgetter("timestamp"), reverse=True
+    )
     index = 0
     while points_to_spend != 0 and index < len(transactions):
-        points = transactions_sorted_by_timestamp[index]["points"]
-        if points_to_spend < points:
-            remainder = points - points_to_spend
+        payer_points = transactions_sorted_by_timestamp[index]["points"]
+        payer = transactions_sorted_by_timestamp[index]["payer"]
+        if points_to_spend < payer_points:
+            remainder = payer_points - points_to_spend
+            points = -transactions_sorted_by_timestamp[index]["points"]
         else:
-            points_to_spend -= points
+            points = -points_to_spend
+            points_to_spend -= payer_points
             remainder = 0
-        payer = transactions_sorted_by_timestamp[index]
-        points = -transactions_sorted_by_timestamp[points]
+
         message.append({"payer": payer, "points": points})
 
         transactions_sorted_by_timestamp[index]["points"] = remainder
+        transactions = transactions_sorted_by_timestamp
         index += 1
+
+        return message
 
 
 def add_points(payer, points_to_add, timestamp):
@@ -40,8 +48,28 @@ def add_points(payer, points_to_add, timestamp):
         {"payer": payer, "points": points_to_add, "timestamp": timestamp}
     )
 
+
 def get_points_by_payer():
-    pass
+    transactions_by_payers = sorted(transactions, key=itemgetter("payer"))
+    points_by_payer = []
+
+    payer = transactions_by_payers[0]["payer"]
+    points = transactions_by_payers[0]["points"]
+    index = 1
+
+    while index < len(transactions_by_payers):
+        if payer == transactions_by_payers[index]["payer"]:
+            points += transactions_by_payers[index]["points"]
+        else:
+            points_by_payer.append({"payer": payer, "points": points})
+            payer = transactions_by_payers[index]["payer"]
+            points = transactions_by_payers[index]["points"]
+        index += 1
+
+    points_by_payer.append({"payer": payer, "points": points})
+
+    return points_by_payer
+
 
 # A route for the root URL path
 @app.route("/", methods=["GET"])
@@ -56,7 +84,7 @@ def add():
         data = json.loads(request.form["text"])
         payer = data["payer"]
         points = int(data["points"])
-        timestamp = data["timestamp"]
+        timestamp = datetime.fromtimestamp(data["timestamp"])
 
         add_points(payer, points, timestamp)
 
@@ -72,7 +100,7 @@ def spend():
     try:
         data = json.loads(request.form["text"])
 
-        points = int(data["points"])
+        points = data["points"]
         if points > total_points:
             message = "You don't have enough points"
             return message, 400
